9e319664874cb4282e05c27cbc576c632ac45b6a
Said <46088969+webmastersaid@users.noreply.github.com>
2023-10-27 14:16:28 +0500

    response

--- app/Http/Controllers/Person/DeleteController.php
+++ app/Http/Controllers/Person/DeleteController.php
@@ -10,6 +10,6 @@ class DeleteController extends Controller
     public function __invoke(Person $person)
     {
         $person->delete();
-        return $person;
+        return response([]);
     }
 }

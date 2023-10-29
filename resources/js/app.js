1a96a32d9c04502098fae2ad471d6aa9c8f5e7bc
Said <46088969+webmastersaid@users.noreply.github.com>
2023-10-29 13:31:06 +0500

    use store

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,9 @@ import './bootstrap'
 import { createApp } from 'vue'
 import App from './components/App.vue'
 import router from './router'
+import store from './store'
 
 const app = createApp(App)
 app.use(router)
-app.mount('#app')
+    .use(store)
+    .mount('#app')
